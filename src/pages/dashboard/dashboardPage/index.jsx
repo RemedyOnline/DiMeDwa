7ca22c21c7c952cf41c25e66893c922ecb1cb9b1
Remedy Online <agyemangmichael555@gmail.com>
2025-01-19 17:29:53 +0000

    SalesByCategory might be the problem

--- src/pages/dashboard/dashboardPage/index.jsx
+++ src/pages/dashboard/dashboardPage/index.jsx
@@ -14,7 +14,7 @@ const DashboardPage = () => {
               <BusinessOverview />
             </div>
             <div className="md:col-span-4 md:grid">
-              <SalesByCategory />
+              {/* <SalesByCategory /> */}
             </div>
           </div>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:pr-4">
