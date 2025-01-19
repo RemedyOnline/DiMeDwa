19a01c7e393631ffc5f589557a06fa653b0e5046
Remedy Online <agyemangmichael555@gmail.com>
2025-01-19 18:16:18 +0000

    after dist folder and commenting chart elements...

--- src/pages/dashboard/dashboardPage/index.jsx
+++ src/pages/dashboard/dashboardPage/index.jsx
@@ -13,18 +13,18 @@ const DashboardPage = () => {
             <div className="md:col-span-2 md:grid">
               <BusinessOverview />
             </div>
-            <div className="md:col-span-4 md:grid">
+            {/* <div className="md:col-span-4 md:grid">
               <SalesByCategory />
-            </div>
+            </div> */}
           </div>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:pr-4">
+          {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:pr-4">
             <div className="col-span-6 md:col-span-3 md:grid">
               <MostViewedProduct />
             </div>
             <div className="col-span-6 md:col-span-3 md:grid">
               <MonthlySales />
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="pt-4 md:col-span-2 md:pt-0">
           <UserEvent />
