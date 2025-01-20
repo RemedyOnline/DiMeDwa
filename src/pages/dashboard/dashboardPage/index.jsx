9858248f5a2c48f64f1b8dbb633eaba80524678c
Remedy Online <agyemangmichael555@gmail.com>
2025-01-20 01:44:20 +0000

    Good bye bbyyyy!

--- src/pages/dashboard/dashboardPage/index.jsx
+++ src/pages/dashboard/dashboardPage/index.jsx
@@ -4,7 +4,7 @@ import UserEvent from "./UserEvent";
 const DashboardPage = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <BusinessOverview />
         <UserEvent />
       </div>
