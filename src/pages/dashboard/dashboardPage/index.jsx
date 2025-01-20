1cef3ea6487cd50432b3f2be80a8e3716e3537e7
Remedy Online <agyemangmichael555@gmail.com>
2025-01-20 01:41:10 +0000

    hmm.. trying again one by one...

--- src/pages/dashboard/dashboardPage/index.jsx
+++ src/pages/dashboard/dashboardPage/index.jsx
@@ -1,11 +1,13 @@
 import BusinessOverview from "./BusinessOverview";
+import MostViewedProduct from "./MostViewedProduct";
 import UserEvent from "./UserEvent";
 
 const DashboardPage = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         <BusinessOverview />
+        <MostViewedProduct />
         <UserEvent />
       </div>
     </section>
